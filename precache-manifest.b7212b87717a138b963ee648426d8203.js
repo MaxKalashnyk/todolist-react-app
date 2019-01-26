ba4d3cbfd4be7b49d46568ed7958afa85b3bd605
kalash14 <maxim.kalashnyk@gmail.com>
2019-01-26 10:48:00 +0200

    Updates

---
+++ precache-manifest.b7212b87717a138b963ee648426d8203.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c6e48ad759bd84ae56d",
+    "url": "/todolist-react-app/static/js/runtime~main.3c6e48ad.js"
+  },
+  {
+    "revision": "d882936c940c19dd4d36",
+    "url": "/todolist-react-app/static/js/main.d882936c.chunk.js"
+  },
+  {
+    "revision": "4510129c8a194a88620a",
+    "url": "/todolist-react-app/static/js/1.4510129c.chunk.js"
+  },
+  {
+    "revision": "d882936c940c19dd4d36",
+    "url": "/todolist-react-app/static/css/main.afb6317c.chunk.css"
+  },
+  {
+    "revision": "a4073f69aeb52cc35cfd21f9ffdc0fc8",
+    "url": "/todolist-react-app/index.html"
+  }
+];
